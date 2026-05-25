ca37259437abe69f7291ce1357e2998cae55d078
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2026-05-25 16:29:53 -0300

    DTOS e Status da task

---
+++ backend/src/main/java/br/gov/sp/attus/backend/dtos/TaskRequestDTO.java
@@ -0,0 +1,41 @@
+package br.gov.sp.attus.backend.dtos;
+
+import br.gov.sp.attus.backend.enums.TaskStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class TaskRequestDTO {
+
+    @NotBlank(message = "Title is required")
+    @Size(max = 100, message = "Title must have at most 100 characters")
+    private String title;
+
+    @Size(max = 500, message = "Description must have at most 500 characters")
+    private String description;
+
+    private TaskStatus status;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+}
---
+++ backend/src/main/java/br/gov/sp/attus/backend/enums/TaskStatus.java
@@ -0,0 +1,7 @@
+package br.gov.sp.attus.backend.enums;
+
+public enum TaskStatus {
+    TODO,
+    IN_PROGRESS,
+    DONE,
+}
