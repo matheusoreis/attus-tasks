118329164ee4fba3c4414c7247a140febdd8d435
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2026-05-25 21:20:00 -0300

    Correção do import do ThemeProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { TaskList } from "./components/TaskList";
 
 import "./index.css";
-import { ThemeProvider } from "./components/theme/provider";
+import { ThemeProvider } from "./components/theme/Provider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
