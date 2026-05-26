b13f418b1267d5fa057ec77ef5d3e023df961452
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2026-05-25 21:08:50 -0300

    OpenApiConfig adicionado ao projeto

---
+++ backend/src/main/java/ai/attus/backend/configs/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package ai.attus.backend.configs;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(
+            new Info()
+                .title("Task API")
+                .version("1.0.0")
+                .description("API de gerenciamento de tarefas")
+        );
+    }
+}
